657af845d651c6c92fe161684e34ef0a658fccb3
wieandk <wieandk@chop.edu>
2025-07-31 16:25:14 -0400

    add large n visualizations for ms analyses

--- R/global.R
+++ R/global.R
@@ -3,4 +3,5 @@ utils::globalVariables(c('allsite_median', 'anomaly_yn', 'attrition_step',
                          'dist_mean', 'dist_mean_sum', 'mad_val', 'mean_val',
                          'median_val', 'n_grp', 'num_pts', 'overall_sd',
                          'sd_val', 'site', 'step0_pts', 'step_number',
-                         'tooltip'))
+                         'tooltip', 'closest_site', 'farthest_site', 'site_w_anom',
+                         'zscr'))
